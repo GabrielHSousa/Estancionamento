a219fbd66a0592ccdb8e6d937d047bd1b22b1b5b
GABRIEL HENRIQUE DE SOUSA <gabriel.hsousa2@senacsp.edu.br>
2022-04-19 19:44:11 -0300

    first commit

---
+++ BancoDeDadosTI13N.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace BancoDeDadosTI13N
+{
+    class Menu
+    {
+        DAO dao;
+        public int opcao;
+        public Menu()
+        {
+            opcao = 0;
+            dao = new DAO("BancoDeDadosTI13N");
+        }//End Constructor.
+
+
+        //Menu!
+        public void MostrarOpcoes()
+        {
+            Console.WriteLine("\n\nEscolha uma das opções abaixo: \n\n" +
+            "\n1. Cadastrar" +
+            "\n2. Consultar Tudo" +
+            "\n3. Consultar Individual" +
+            "\n4. Atualizar" +
+            "\n5. Excluir");
+
+            opcao = Convert.ToInt32(Console.ReadLine());
+        }//End Menu.
+
+
+
+        public void Executar() 
+        {
+            do
+
+            {
+                MostrarOpcoes(); //Mostrando o menu para o cliente/usuário
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Informe seu nome: ");
+                        string nome = Console.ReadLine();
+                        Console.WriteLine("\nInforme seu telefone: ");
+                        string telefone = Console.ReadLine();
+                        Console.WriteLine("\nInforme seu endereço: ");
+                        string endereco = Console.ReadLine();
+                        //Executar o método inserir
+                        dao.Inserir(nome, telefone, endereco);
+                        break;
+                    case 2:
+                        //CONSULTAR OS DADOS 
+                        Console.WriteLine(dao.ConsultarTudo());
+                        break;
+                    case 3:
+                        // CONSULTAR INDIVIDUAL
+                        Console.WriteLine("Informe o codigo que deseja Consultar");
+                        int codigo = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Nome:" + dao.ConsultarNome(codigo) + "\nTelefone:" + dao.ConsultarTelefone(codigo) + "\nEndereço:" + dao.ConsultarEndereco(codigo));
+                        break;
+                    case 4:
+                        // ATUALIZAR
+                        Console.WriteLine("Qual tabela deseja atualizar?");
+                        string campo = Console.ReadLine();
+                        Console.WriteLine("Qual o novo dado?");
+                        string novoDado = Console.ReadLine();
+                        Console.WriteLine("Qual o codigo da pessoa que deseja atualizar?");
+                        codigo = Convert.ToInt32(Console.ReadLine());
+                        dao.Atualizar(campo, novoDado, codigo);
+                        break;
+                    case 5:
+                        //DELETAR
+                        Console.WriteLine("Informe o codigo que deseja deletar");
+                        codigo = Convert.ToInt32(Console.ReadLine());
+                        //USAR O METODO DA CLASSE DAO
+                        dao.Deletar(codigo);
+                        break;
+                    case 0:
+                        Console.WriteLine("Obrigado!");
+                        break;
+                    default:
+                        Console.WriteLine("Codigo digitado não válido!");
+                        break;
+                }// End Switch Case.
+            } while (opcao != 0);
+        }//End Method.
+    }//FIM DA CLASSE
+}//FIM DO POGRAMA
---
+++ DAO.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;//Imports para conexão com o banco de dados
+using MySql.Data.MySqlClient;//Imports para realizar comandos no banco
+
+namespace BancoDeDadosTI13N
+{
+    class DAO
+    {
+        MySqlConnection conexao;
+        public string dados;
+        public string resultado;
+        // declara
+        public int[] cod;
+        public string[] nome;
+        public string[] telefone;
+        public string[] endereco;
+        public int i;
+        public string msg;
+        public int contador = 0;
+        //Contrutor
+        public DAO(string nomeDoBancoDeDados)
+        {
+            conexao = new MySqlConnection("server=localhost;DataBase=" + nomeDoBancoDeDados + ";Uid=root;Password=;");
+            try
+            {
+                conexao.Open();//Solicitando a entrada ao banco de dados
+                Console.WriteLine("Entrei!!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Algo deu errado!\n\n" + e);
+                conexao.Close();//Fechando a conexão com banco de dados
+            }//fim da tentativa de conexão com o banco de dados
+        }//fim do construtor
+
+        //Criar o método INSERIR
+        public void Inserir(string nome, string telefone, string endereco)
+        {
+            try
+            {
+                dados = "('','" + nome + "','" + telefone + "','" + endereco + "')";
+                resultado = "Insert into Pessoa(codigo, nome, telefone, endereco) values" + dados;
+                //Executar o comando resultado no banco de dados
+                MySqlCommand sql = new MySqlCommand(resultado, conexao);
+                resultado = "" + sql.ExecuteNonQuery();
+                Console.WriteLine(resultado + " Linha(s) Afetada(s)!");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Algo deu errado!\n\n" + e);
+
+            }//fim do catch
+        }//fim do método inserir
+
+
+        public void PreencherVetor()
+        {
+            string query = "select * from pessoa";
+            cod = new int[100];
+            nome = new string[100];
+            telefone = new string[100];
+            endereco = new string[100];
+
+
+            for ( i = 0; i < 100; i++)
+            {
+                cod[i] = 0;
+                nome[i] = "";
+                telefone[i] = "";
+                endereco[i] = "";
+
+            }//FIM DA REPETIÇÃO
+
+
+        //CRIAR COMANDO
+
+        MySqlCommand coletar = new MySqlCommand(query,conexao);
+        //usar comando lendo os dados do banco
+        MySqlDataReader leitura = coletar.ExecuteReader();
+
+            i = 0;
+            while(leitura.Read())
+            {
+                cod[i] = Convert.ToInt32(leitura["codigo"]);
+                nome[i] = leitura["nome"] + "";
+                telefone[i] = leitura["telefone"] + "";
+                endereco[i] = leitura["endereco"] + "";
+                i++;
+                contador++;
+            }//Fim do while
+
+            //FECHAR O DATAREADER
+            leitura.Close();
+
+        }//FIM DO PREECHER VETOR
+
+        public string ConsultarTudo()
+        {
+            //PREECHER O VETOR
+            PreencherVetor();
+            msg = "";
+            for(int i = 0; i < contador; i++)
+            {
+                msg += "\n\ncodigo:" + cod[i] + "Nome: " + nome[i] + ",Telefone" + telefone[i] + ", Endereço:" + endereco[i];
+            }//FIM DO FOR
+            return msg;
+
+        }//FIM DO CONSULTAR TUDO
+        public string ConsultarNome(int codigo)
+        {
+            PreencherVetor();
+            for(int i=0; i < contador; i++)
+            {
+                if(codigo == cod[i])
+                {
+                    return nome[i];
+                }
+            }//FIM  DO FOR
+            return "codigo não encotrada!";
+        }// FIM DO CONSULTARNOME
+        public string ConsultarTelefone(int codigo)
+        {
+            PreencherVetor();
+            for (int i = 0; i < contador; i++)
+            {
+                if (codigo == cod[i])
+                {
+                    return telefone[i];
+                }
+            }//FIM  DO FOR
+            return "codigo não encotrada!";
+        }// FIM DO CONSULTARTELEFONE
+        public string ConsultarEndereco(int codigo)
+        {
+            PreencherVetor();
+            for (int i = 0; i < contador; i++)
+            {
+                if (codigo == cod[i])
+                {
+                    return endereco[i];
+                }
+            }//FIM  DO FOR
+            return "codigo não encotrada!";
+        }// FIM DO CONSULTARENDERECO
+        public void Atualizar(string campo, string novoDado, int codigo)
+        {
+            try
+            {
+                resultado = "update pessoa set " + campo + " = '" + novoDado + "' where codigo ='" + codigo + "'";
+                // EXECULTAR O SCRIPT
+                MySqlCommand sql = new MySqlCommand(resultado, conexao);
+                resultado = "" + sql.ExecuteNonQuery();
+                Console.WriteLine("Dado Atualizado com Sucesso!");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Algo deu Errado!" + e);
+            }
+        }//FIM DO ATUALIZAR
+        public void Deletar(int codigo)
+        {
+            resultado = "delete from pessoa where codigo = '" + codigo + "'";
+            // EXECULTAR O COMANDO
+            MySqlCommand Sql = new MySqlCommand(resultado, conexao);
+            resultado = "" + Sql.ExecuteNonQuery();
+            //MENSAGEM
+            Console.WriteLine("Dados Excluido com Sucesso!");
+
+        }// FIM DO DELETAR
+    }//fim da classe
+}//fim do projeto
